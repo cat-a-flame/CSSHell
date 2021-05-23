c2a02d7e7046d2ccf33d64a01c773a36f9911222
Mátyás Budavári <budavariam@gmail.com>
2021-05-23 08:35:50 +0200

    Merge together sass watch and serve

--- .eleventy.js
+++ .eleventy.js
@@ -3,18 +3,16 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const filters = require('./_11ty/filters.js');
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addWatchTarget('src/sass');
   eleventyConfig.addPassthroughCopy('src/images');
   eleventyConfig.addPassthroughCopy('src/js');
+  eleventyConfig.addPassthroughCopy({'src/css': 'css'});
 
   // Filters
   Object.keys(filters).forEach(filterName => {
     eleventyConfig.addFilter(filterName, filters[filterName])
   });
 
-  // sass config
-  eleventyConfig.setBrowserSyncConfig({
-    files: './_site/css/**/*.css'
-  });
 
   // Plugins
   eleventyConfig.addPlugin(pluginRss);
